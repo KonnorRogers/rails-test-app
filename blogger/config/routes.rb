a48b478ca850ccab1420ba33f69e93ee05c19e77
ParamagicDev <konnor5456@gmail.com>
2017-12-31 18:35:47 -0500

    Sorcery Authentication complete

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -4,6 +4,12 @@ Rails.application.routes.draw do
       resources :comments
   end
   resources :tags
+  resources :authors
+
+  resources :author_sessions, only: [:new, :create, :destroy]
+
+  get "login" => "author_sessions#new"
+  get "logout" => "author_sessions#destroy"
   # resource named articles
   #router now expects requests following RESTful model
   #REpresentational State Transfer
